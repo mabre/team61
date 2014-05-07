19c7af5f019b224b45024509865cb96a089d0e34
Simon Franz <simon.franz@hhu.de>
2014-05-07 20:25:30 +0200

    +Board

--- src/de/hhu/propra/team61/Afrobob.java
+++ src/de/hhu/propra/team61/Afrobob.java
@@ -1,8 +1,17 @@
 package de.hhu.propra.team61;
 
-/**
+/*
  * Created by dinii on 15.04.14.
+ * ProPra Team 61:
+ * Markus Brenneis (Mat.Nr.) Marbre
+ * Jan Ecknigk 2202505 (GitName)
+ * Jessica (Nachname/Mat.Nr./GitName)
+ * Kevin Gnyp (Mat.Nr/GitName)
+ * Simon Franz 2204765 Git: DiniiAntares
+ * Project: Worms clone
+ *
  */
+
 public class Afrobob {
     public static void main(String[] args) {
 
