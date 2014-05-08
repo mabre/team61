809decfe358fc4ea3b734c0876e6c5320c9a589b
Kevin Bernard Gnyp <kevin.gnyp@uni-duesseldorf.de>
2014-05-08 21:24:51 +0200

    Draw the txt-File in a Window

---
+++ src/de/hhu/propra/team61/Mapwindow.java
@@ -0,0 +1,51 @@
+package de.hhu.propra.team61;
+
+import de.hhu.propra.team61.IO.TerrainLoader;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+/**
+ * Created by kegny on 08.05.14.
+ * This class is supposed to draw the Array given by "TerrainLoader" rendering the Map visible.
+ */
+public class Mapwindow extends Application{
+    public static void main(String[] args){
+         launch(args);
+    }
+
+    public void start(Stage primaryStage) {
+        ArrayList<ArrayList<Character>> terrain = TerrainLoader.load("Something"); //Waiting for actual implementation of this method
+
+        //Draw Terrain
+        GridPane grid = new GridPane();
+        grid.setGridLinesVisible(true); //Gridlines on/off
+        grid.setAlignment(Pos.CENTER);
+        grid.setPadding(new Insets(10, 10, 10, 10));
+
+        for(int i=0; i < terrain.size(); i++){
+            for(int j=0; j < terrain.get(i).size(); j++){
+                Label test = new Label(terrain.get(i).get(j)+""); //+"" Makes the Char a String to avoid type mismatches
+                grid.add(test,j,i);
+            }
+        }
+
+        StackPane root = new StackPane();
+        root.getChildren().add(grid);
+
+        Scene zeichnung = new Scene(root, 800, 600);
+
+        primaryStage.setTitle("The Playground");
+        primaryStage.setScene(zeichnung);
+        primaryStage.show();
+    }
+}
