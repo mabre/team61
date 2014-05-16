15a82fd612c9c431678c731b86eddac618c63dad
Kevin Bernard Gnyp <kevin.gnyp@uni-duesseldorf.de>
2014-05-16 13:27:36 +0200

    ReUpload

--- src/de/hhu/propra/team61/Objects/Figure.java
+++ src/de/hhu/propra/team61/Objects/Figure.java
@@ -1,5 +1,7 @@
 package de.hhu.propra.team61.Objects;
 
+import de.hhu.propra.team61.IO.JSON.JSONObject;
+
 /**
  * Created by kevgny on 14.05.14.
  */
@@ -13,35 +15,52 @@ public class Figur {
     private boolean isPoisoned;
     private boolean isStuck;
 
-
     // In and Out
-    public Figur(String name, int hp, int armor){
+    public Figur(String name, int hp, int armor, boolean isBurning, boolean isPoisoned, boolean isStuck){
         this.name   = name;
         this.health = hp;
         this.armor  = armor;
-    }
-    public Figur(){ //from Json
 
+        this.isBurning  = isBurning;
+        this.isPoisoned = isPoisoned;
+        this.isStuck    = isStuck;
+    }
+    public Figur(JSONObject input){
+        this.name = input.getString("name");
+        this.health = input.getInt("health");
+        this.armor  = input.getInt("armor");
+        this.isBurning  = input.getBoolean("isBurning");
+        this.isPoisoned = input.getBoolean("isPoisoned");
+        this.isStuck    = input.getBoolean("isStuck");
+    }
+    public JSONObject toJson(){
+        JSONObject output = new JSONObject();
+        output.put("name", name);
+        output.put("health", health);
+        output.put("armor", armor);
+
+        output.put("isBurning", isBurning);
+        output.put("isPoisoned", isPoisoned);
+        output.put("isStuck",isStuck);
+        return output;
     }
 
-    public static void toJson(){
-
-    }
 
     // Getter and Setter
     public String getName(){return name;}
     public void setName(String name){this.name = name;}
 
-    public int  getHealth() {return health;}
+    public int getHealth() {return health;}
     public void setHealth(int health) {this.health = health;}
 
-    public int  getArmor() {return armor;}
+    public int getArmor() {return armor;}
     public void setArmor(int armor) {this.armor = armor;}
+
 
     public boolean getIsBurning() {return isBurning;}
     public void setIsBurning(boolean isBurning){this.isBurning = isBurning;}
-
     public boolean getIsPoisoned() {return isPoisoned;}
+
     public void setIsPoisoned(boolean isPoisoned){this.isPoisoned = isPoisoned;}
 
     public boolean getIsStuck() {return isStuck;}
@@ -50,7 +69,6 @@ public class Figur {
 
 
     //For testing purposes only
-
     private static void printAllAttributes(Figur testwurm){
         System.out.println("Health  : " + testwurm.getHealth());
         System.out.println("Armor   : " + testwurm.getArmor());
@@ -60,16 +78,26 @@ public class Figur {
         System.out.println("Stuck   : " + testwurm.getIsStuck());
         System.out.println();
     }
-
     public static void main(String[] args){
-        //Constructor A test
-        Figur testwurm = new Figur("Stig",100,36);
-        printAllAttributes(testwurm);
+        System.out.println("Var-Constructor-Test");
+        System.out.println("-------------------------");
+        Figur testwurmA = new Figur("Stig",100,36,false,false,true);
+        printAllAttributes(testwurmA);
 
-        //Setter Test
-        testwurm.setName("The Stig");
-        testwurm.setArmor(8);
-        testwurm.setHealth(20);
-        printAllAttributes(testwurm);
+        System.out.println("Getter-Setter-Test");
+        System.out.println("-------------------------");
+        testwurmA.setName("The " + testwurmA.getName());
+        testwurmA.setArmor(testwurmA.getArmor() - 8);
+        testwurmA.setHealth(testwurmA.getHealth() - 50);
+        testwurmA.setIsBurning(!testwurmA.getIsBurning());
+        testwurmA.setIsPoisoned(!testwurmA.getIsPoisoned());
+        testwurmA.setIsStuck(!testwurmA.getIsStuck());
+        printAllAttributes(testwurmA);
+
+        System.out.println("JSON-Constructor-IO-Test");
+        System.out.println("-------------------------");
+        System.out.println("JSON: "+testwurmA.toJson().toString());
+        Figur testwurmB = new Figur(testwurmA.toJson());
+        printAllAttributes(testwurmB);
     }
 }
