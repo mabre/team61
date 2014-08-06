94833a6fb2fd6b0437504aa2245afafa08af4acb
Markus Brenneis <Markus.Brenneis@uni-duesseldorf.de>
2014-08-06 10:04:50 +0200

    make batch file work in read-only dirs bat -> cmd as we want to support NT only (Java 8 requires Vista anyway)

--- Launch.cmd
+++ Launch.cmd
@@ -7,7 +7,17 @@ echo =========================
 set size=0
 call :filesize "Afrobob.log"
 echo file size is %size%
-java -jar Charly_in_Madagascar.jar >> Afrobob.log 2>&1
+set writable=0
+copy /y Launch.bat .writable > NUL 2>&1 && set writable=1
+echo %writable%
+if "%writable%" == "1" (
+	del .writable
+	java -jar Charly_in_Madagascar.jar >> Afrobob.log 2>&1
+
+) else (
+	echo Cannot write to .
+	java -jar Charly_in_Madagascar.jar
+)
 goto :eof
 
 :: get file size TODO use it and make it more like the linux script (date logging etc.)
