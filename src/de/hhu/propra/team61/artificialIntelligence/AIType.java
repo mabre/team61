4d056a24ab525d57609be21b22b6f1c739062de5
Markus Brenneis <Markus.Brenneis@uni-duesseldorf.de>
2014-08-29 21:59:11 +0200

    AIType enum missing

---
+++ src/de/hhu/propra/team61/artificialIntelligence/AIType.java
@@ -0,0 +1,15 @@
+package de.hhu.propra.team61.artificialIntelligence;
+
+/**
+ * Created by markus on 29.08.14. TODO IMPORTANT
+ */
+public enum AIType {
+    NULL(0),
+    SIMPLE(1);
+
+    private int value;
+
+    private AIType(int value) {
+        this.value = value;
+    }
+};
